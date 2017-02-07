11ad27d9fb3a64afcf2f1d1e6a316a11061e54db
bigO <coins@Nhas-MBP.net.fpt>
2017-02-07 12:53:55 +0700

    beauty code

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,67 +1,60 @@
 'use-strict'
 
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  ListView,
-} from 'react-native';
-
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, ListView} from 'react-native';
 
 var catelogryData = [
-  {title:'Map', key:'1'},
-  {title:'Group', key:'2'}
+    {
+        title: 'Map',
+        key: '1'
+    }, {
+        title: 'Group',
+        key: '2'
+    }
 ];
 
-class  Menu extends Component {
+class Menu extends Component {
 
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(catelogryData),
-    };
-  }
+    constructor() {
+        super();
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.state = {
+            dataSource: ds.cloneWithRows(catelogryData)
+        };
+    }
 
-  render () {
-    return (
-      <View style={[styles.container,]}>
-
-        <ListView  contentContainerStyle={styles.listviewCenter}
-            dataSource={this.state.dataSource}
-            renderRow={this.createButtonCatelogry.bind(this)}/>
-
-        </View>
-      ); // end return
+    render() {
+        return (
+            <View style={[styles.container]}>
+                <ListView contentContainerStyle={styles.listviewCenter} dataSource={this.state.dataSource} renderRow={this.createButtonCatelogry.bind(this)}/>
+            </View>
+        ); // end return
     } // end render
 
     // btnMapOnPress
     btnMapOnPress(key) {
-      switch(key) {
-        case '1':
-
-          break;
-          default:
-
-          break;
-      }
+        switch (key) {
+            case '1':
+                break;
+            default:
+                break;
+        }
 
     } // end btnMapOnPress
 
     // createButtonCatelogry
     createButtonCatelogry(property) {
-      return(
-        <TouchableOpacity
-          style={[styles.btnCatelogry,styles.containerCenter]}
-          onPress={this.btnMapOnPress.bind(this, property.key)}>
-          <Text style={[styles.btnCatelogryTitle]}>
-            {property.title}
-          </Text>
-        </TouchableOpacity>
-      );
+        return (
+            <TouchableOpacity style={[styles.btnCatelogry, styles.containerCenter]} onPress={this.btnMapOnPress.bind(this, property.key)}>
+                <Text style={[styles.btnCatelogryTitle]}>
+                    {property.title}
+                </Text>
+            </TouchableOpacity>
+        );
     } // end createButtonCatelogry
 
-  } // end class
+} // end class
 
-  module.exports = Menu;
+module.exports = Menu;
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,21 +1,34 @@
 'use-strict'
 
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Navigator} from 'react-native';
 
 import Menu from '../components/menu'
 import Map from '../components/map'
 
 export default class XMGroups extends Component {
 
-  render() {
-    return (
-      <Map>
-      </Map>
-    ); // end return
-  } // end render
+    render() {
+        return (<Navigator initialRoute={{
+            name: 'menuScreen'
+        }} renderScene={this.renderScene}/>); // end return
+    } // end render
+
+    renderScene(route, navigator) {
+        switch (route.name) {
+            case 'menuScreen':
+                return (
+                    <Menu></Menu>
+                );
+            case 'mapScreen':
+                return (
+                    <Map></Map>
+                );
+            default:
+                return (
+                    <View></View>
+                );
+        }
+    }
 
 }
